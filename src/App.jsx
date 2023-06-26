851ca5c1a9dfe35cd9863515f60e94f1559426ad
sjfortin <sam.j.fortin@gmail.com>
2023-06-25 21:53:32 -0500

    add analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { inject } from "@vercel/analytics";
 import ElevenEighty from "./components/ElevenEighty";
 
 function App() {
+  inject();
+
   return (
     <>
-      <h1 className="px-4 mt-5 mb-2 text-4xl flex justify-center">Sam Fortin</h1>
+      <h1 className="px-4 mt-5 mb-2 text-4xl flex justify-center">
+        Sam Fortin
+      </h1>
       <div className="flex justify-center mb-5">
         <a
           className="text-gray-400 text-sm hover:text-gray-900"
